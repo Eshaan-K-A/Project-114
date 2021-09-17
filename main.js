3f4e1e04770ed0c3f119e213489ad75b9d78a868
Eshaan <78539313+Eshaan-K-A@users.noreply.github.com>
2021-09-17 18:17:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload() {
+
+}
+function setup() {
+    canvas = createCanvas(330, 300);
+    canvas.center();
+}
+function draw() {
+
+}
+function clickThePhoto() {
+    save("MyMoustacheImage.png");
+}
